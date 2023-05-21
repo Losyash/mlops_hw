aa7149cd4326ccff2405736b965829d65172fc5d
Alexey Naymushin <alexeynaymushin@urfu.ru>
2023-05-21 11:57:56 +0500

    Change bash script.

--- lab1/pipeline.sh
+++ lab1/pipeline.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+if conda env list | grep -q lab1; then echo "Environment already exists"; else conda create -y -n lab1 python=3.10; fi
+
+conda activate lab1
+
 cd ~/mlops/lab1
 
-if ! [ -d data ]; then
-    mkdir data
-fi
 
-python data_creation.py
-python data_preprocessing.py
-python model_preparation.py
-python model_testing.py
+if ! [ -d data ]; then mkdir data fi
+
+# python data_creation.py
+# python data_preprocessing.py
+# python model_preparation.py
+# python model_testing.py
