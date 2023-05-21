eaabd6f1d0cee09b61998c882c8c7d2e8da18999
Alexey Naymushin <alexeynaymushin@urfu.ru>
2023-05-21 12:27:06 +0500

    Change bash script.

--- lab1/pipeline.sh
+++ lab1/pipeline.sh
@@ -11,7 +11,7 @@ cd ~/mlops/lab1
 
 if ! [ -d data ]; then mkdir data; fi
 
-# python data_creation.py
-# python data_preprocessing.py
-# python model_preparation.py
-# python model_testing.py
+python data_creation.py
+python data_preprocessing.py
+python model_preparation.py
+python model_testing.py
