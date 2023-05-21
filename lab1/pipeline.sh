dbc88f68cdcbe88ce4a6d216a489b41368482608
Alexey Naymushin <alexeynaymushin@urfu.ru>
2023-05-21 09:56:34 +0500

    Change bash script.

--- lab1/pipeline.sh
+++ lab1/pipeline.sh
@@ -1,1 +1,15 @@
-# pipeline.sh
+#!/bin/bash
+
+conda activate ~/mlops/lab1/conda
+pip install gdown, zipfile36, split-folders, tensorflow
+
+cd ~/mlops/lab1
+
+if ! [ -d data ]; then
+    mkdir data
+fi
+
+python data_creation.py
+python data_preprocessing.py
+python model_preparation.py
+python model_testing.py
--- lab1/settings.py
+++ lab1/settings.py
@@ -1,6 +1,6 @@
 DATA_URL = 'https://drive.google.com/file/d/18vilMtixEDJns6f-iVt6-xM9BhOAtNiC/view?usp=share_link'
 
-DATASET_PATH = 'lab1/data'
+DATASET_PATH = 'data'
 DATASET_FILENAME = 'faces.zip'
 
 DATASET_RAW_FOLDER = 'faces_raw' 
@@ -9,7 +9,7 @@ DATASET_SPLITTED_FOLDER = 'faces_splitted'
 DATASET_TRAIN_FOLDER = 'train';
 DATASET_TEST_FOLDER = 'val';
 
-MODEL_PATH = 'lab1/model'
+MODEL_PATH = 'model'
 
 IMAGE_HEIGHT = 180
 IMAGE_WIDTH = 180
