b44a80da26f33c9a111c386ced2fc604976ab018
Alexey Naymushin <alexeynaymushin@urfu.ru>
2023-05-21 12:23:43 +0500

    Change bash script.

--- lab1/pipeline.sh
+++ lab1/pipeline.sh
@@ -3,14 +3,13 @@
 if conda env list | grep -q lab1; then echo "Environment already exists"; else conda create -y -n lab1 python=3.10; fi
 
 source ~/miniconda3/etc/profile.d/conda.sh
-conda deactivate
 conda activate lab1
 
 pip install zipfile36 split-folders tensorflow pillow
 
 cd ~/mlops/lab1
 
-if ! [ -d data ]; then mkdir data fi
+if ! [ -d data ]; then mkdir data; fi
 
 # python data_creation.py
 # python data_preprocessing.py
