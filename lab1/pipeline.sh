6eab74b9ff15b0b9d66868874d22bb4b307f0774
Alexey Naymushin <alexeynaymushin@urfu.ru>
2023-05-21 12:13:23 +0500

    Fixed code.

--- lab1/pipeline.sh
+++ lab1/pipeline.sh
@@ -2,10 +2,10 @@
 
 if conda env list | grep -q lab1; then echo "Environment already exists"; else conda create -y -n lab1 python=3.10; fi
 
+source ~/miniconda3/etc/profile.d/conda.sh
 conda activate lab1
 
 cd ~/mlops/lab1
-
 
 if ! [ -d data ]; then mkdir data fi
 
